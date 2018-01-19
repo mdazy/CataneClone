b6c6bbe28f6387c88a653272e37e3aa736f712a3
dazy <matthieu.dazy@pdgm.com>
2018-01-19 16:25:35 +0100

    Lighter player colors.

--- viewUtils.h
+++ viewUtils.h
@@ -5,5 +5,5 @@
 #include "board.h"
 
 const float degToRad = 3.1415926 / 180.0;
-const QColor playerColor[ 4 ] = { Qt::red, Qt::green, Qt::blue, QColor( 255, 127, 0.0 ) };
+const QColor playerColor[ 4 ] = { QColor( 255, 127, 127 ), QColor( 127, 255, 127 ), QColor( 127, 127, 255 ), QColor( 255, 180, 0 ) };
 const QColor tileColor[ Hex::nbTypes ] = { Qt::red, Qt::darkGreen, Qt::yellow, Qt::green, Qt::gray, Qt::darkYellow, Qt::blue };
