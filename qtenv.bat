50e70f80d9748fd8adbcf85e091c21d423c7837a
Matthieu Dazy <matthieu.dazy@emerson.com>
2019-01-28 09:38:49 +0100

    Fix path to new local Qt

--- qtenv.bat
+++ qtenv.bat
@@ -1,4 +1,4 @@
 @echo off
-set QTDIR=<your Qt install here>
+set QTDIR=D:\Work\SKUA\P4Toolkits\Qt5\Qt-5.9.4\Win_x64_6.1_v15_debug
 set QT_QPA_PLATFORM_PLUGIN_PATH=%QTDIR%\plugins
 set PATH=%PATH%;%QTDIR%\bin
